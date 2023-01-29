10aff4f026e600806dd86c33453f8ab2ff62f57b
mhafizkn <hafizkn98@gmail.com>
2023-01-29 14:24:53 +0800

    Proper implementation using Controller, Model & Service

--- src/main/java/com/microservice/TaxCalculator/model/IncomeTax.java
+++ src/main/java/com/microservice/TaxCalculator/model/IncomeTax.java
@@ -1,5 +1,6 @@
 package com.microservice.TaxCalculator.model;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -8,10 +9,33 @@ public class IncomeTax {
     private double annualIncome;
     private double taxAmount;
     private String taxSlab;
-    private List<String> taxClaimOptions;
+    // Declare array of taxClaimOptions as static to initialize the data
+    private static List<String> taxClaimOptions = Arrays.asList(
+            "Self and dependent (RM9000)",
+            "Medical Expenses for parents (RM8000)",
+            "Basic Supporting Equipment (RM6000)",
+            "Disabled Individual (RM6000)",
+            "Education fees (RM7000)",
+            "Medical Expenses on Serious Disease (RM8000)",
+            "Lifestyle (RM2500)",
+            "Lifestyle on personal computer, smartphone or table (RM2500)",
+            "Breastfeeding Equipment (RM1000)",
+            "Child Care Fee (RM3000)",
+            "SSPN (RM8000)",
+            "Husband/ Wife/ Alimony Payments (RM4000)",
+            "Disabled spouse (RM5000)",
+            "Education fee for children (RM8000)",
+            "Disabled Child (RM6000)",
+            "EPF (RM7000)",
+            "PRS (RM3000)",
+            "Education or Medical Insurance (RM3000)",
+            "SOCSO (RM250)",
+            "Lifestyle on Sport Equipment (RM500)",
+            "Domestic Travel (RM1000)"
+    );
 
-    public IncomeTax(List<String> taxClaimOptions) {
-        this.taxClaimOptions = taxClaimOptions;
+    public void setAnnualIncome(double annualIncome) {
+        this.annualIncome = annualIncome;
     }
 
     public double getTaxAmount() {
@@ -40,10 +64,15 @@ public class IncomeTax {
 
     @Override
     public String toString() {
-        return "IncomeTax{" +
-                ", taxAmount=" + taxAmount +
-                ", taxSlab='" + taxSlab + '\'' +
-                ", taxClaimOptions=" + taxClaimOptions +
+//        return "IncomeTax{" +
+//                ", taxAmount=" + taxAmount +
+//                ", taxSlab='" + taxSlab + '\'' +
+//                ", taxClaimOptions=" + taxClaimOptions +
+//                '}';
+
+        return "{\"taxAmount\": " + taxAmount +
+                ", \"taxSlab\": \"" + taxSlab + "\"" +
+//                "\"taxClaimOptions\"" + taxClaimOptions
                 '}';
     }
 }
--- src/main/java/com/microservice/TaxCalculator/service/TaxCalculator.java
+++ src/main/java/com/microservice/TaxCalculator/service/TaxCalculator.java
@@ -9,75 +9,52 @@ import java.util.List;
 @Service
 public class TaxCalculator {
 
-    static List<String> taxClaimOptions = Arrays.asList(
-                "Self and dependent (RM9000)",
-                "Medical Expenses for parents (RM8000)",
-                "Basic Supporting Equipment (RM6000)",
-                "Disabled Individual (RM6000)",
-                "Education fees (RM7000)",
-                "Medical Expenses on Serious Disease (RM8000)",
-                "Lifestyle (RM2500)",
-                "Lifestyle on personal computer, smartphone or table (RM2500)",
-                "Breastfeeding Equipment (RM1000)",
-                "Child Care Fee (RM3000)",
-                "SSPN (RM8000)",
-                "Husband/ Wife/ Alimony Payments (RM4000)",
-                "Disabled spouse (RM5000)",
-                "Education fee for children (RM8000)",
-                "Disabled Child (RM6000)",
-                "EPF (RM7000)",
-                "PRS (RM3000)",
-                "Education or Medical Insurance (RM3000)",
-                "SOCSO (RM250)",
-                "Lifestyle on Sport Equipment (RM500)",
-                "Domestic Travel (RM1000)"
-    );
-
     public IncomeTax calculateTax(double annualIncome) {
 
         double taxAmount;
         String taxSlab;
 
-        IncomeTax incomeTax = new IncomeTax(taxClaimOptions);
-
+        // Calculate the income tax and display the tax slab based on the annual income
         if (annualIncome <= 5000) {
             taxAmount = 0;
-            taxSlab = "0 - 5000";
+            taxSlab = "0.00 - 5000.00";
         } else if (annualIncome > 5001 && annualIncome <= 20000) {
             taxAmount = 0 + (annualIncome - 5000);
-            taxSlab = "5,001 - 20,000";
+            taxSlab = "5,001.00 - 20,000.00";
         } else if (annualIncome > 20001 && annualIncome <= 35000) {
             taxAmount = 150 + (annualIncome - 20000) * 0.03;
-            taxSlab = "20,001 - 35,000";
+            taxSlab = "20,001.00 - 35,000.00";
         } else if (annualIncome > 35001 && annualIncome <= 50000) {
             taxAmount = 600 + (annualIncome - 35000) * 0.08;
-            taxSlab = "35,001 - 50,000";
+            taxSlab = "35,001.00 - 50,000.00";
         } else if (annualIncome > 50001 && annualIncome <= 70000) {
             taxAmount = 1800 + (annualIncome - 50000) * 0.13;
-            taxSlab = "50,001 - 70,000";
+            taxSlab = "50,001.00 - 70,000.00";
         } else if (annualIncome > 70001 && annualIncome <= 100000) {
             taxAmount = 4400 + (annualIncome - 70000) * 0.21;
-            taxSlab = "70,001 - 100,000";
+            taxSlab = "70,001.00 - 100,000.00";
         } else if (annualIncome > 100001 && annualIncome <= 250000) {
             taxAmount = 10700 + (annualIncome - 100000) * 0.24;
-            taxSlab = "70,001 - 100,000";
+            taxSlab = "100,001.00 - 250,000.00";
         } else if (annualIncome > 250001 && annualIncome <= 400000) {
             taxAmount = 46700 + (annualIncome - 250000) * 0.245;
-            taxSlab = "250,001 - 400,000";
+            taxSlab = "250,001.00 - 400,000.00";
         } else if (annualIncome > 400001 && annualIncome <= 600000) {
             taxAmount = 83450 + (annualIncome - 400000) * 0.25;
-            taxSlab = "250,001 - 400,000";
+            taxSlab = "400,001.00 - 600,000.00";
         } else if (annualIncome > 600001 && annualIncome <= 1000000) {
             taxAmount = 133450 + (annualIncome - 600000) * 0.26;
-            taxSlab = "600,001 - 1,000,000";
+            taxSlab = "600,001.00 - 1,000,000.00";
         } else if (annualIncome > 1000001 && annualIncome <= 2000000) {
             taxAmount = 237450 + (annualIncome - 1000000) * 0.28;
-            taxSlab = "1,000,001 - 2,000,000";
+            taxSlab = "1,000,001.00 - 2,000,000.00";
         } else {
             taxAmount = 517450 + (annualIncome - 2000000) * 0.3;
-            taxSlab = "Exceed 2,00,000";
+            taxSlab = "Exceed 2,00,000.00";
         }
 
+        // Create & store the object - income tax
+        IncomeTax incomeTax = new IncomeTax();
         incomeTax.setTaxAmount(taxAmount);
         incomeTax.setTaxSlab(taxSlab);
 
