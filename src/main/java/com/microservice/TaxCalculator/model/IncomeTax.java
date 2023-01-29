2bd99f21351c91c0a23b2f382667a786a0e4ffa2
mhafizkn <hafizkn98@gmail.com>
2023-01-29 17:24:09 +0800

    Declare static array of tax claim options

--- src/main/java/com/microservice/TaxCalculator/model/IncomeTax.java
+++ src/main/java/com/microservice/TaxCalculator/model/IncomeTax.java
@@ -10,29 +10,29 @@ public class IncomeTax {
     private double taxAmount;
     private String taxSlab;
     // Declare array of taxClaimOptions as static to initialize the data
-//    private static List<String> taxClaimOptions = Arrays.asList(
-//            "Self and dependent (RM9000)",
-//            "Medical Expenses for parents (RM8000)",
-//            "Basic Supporting Equipment (RM6000)",
-//            "Disabled Individual (RM6000)",
-//            "Education fees (RM7000)",
-//            "Medical Expenses on Serious Disease (RM8000)",
-//            "Lifestyle (RM2500)",
-//            "Lifestyle on personal computer, smartphone or table (RM2500)",
-//            "Breastfeeding Equipment (RM1000)",
-//            "Child Care Fee (RM3000)",
-//            "SSPN (RM8000)",
-//            "Husband/ Wife/ Alimony Payments (RM4000)",
-//            "Disabled spouse (RM5000)",
-//            "Education fee for children (RM8000)",
-//            "Disabled Child (RM6000)",
-//            "EPF (RM7000)",
-//            "PRS (RM3000)",
-//            "Education or Medical Insurance (RM3000)",
-//            "SOCSO (RM250)",
-//            "Lifestyle on Sport Equipment (RM500)",
-//            "Domestic Travel (RM1000)"
-//    );
+    private static List<String> taxClaimOptions = Arrays.asList(
+            "Self and dependent (RM9000)",
+            "Medical Expenses for parents (RM8000)",
+            "Basic Supporting Equipment (RM6000)",
+            "Disabled Individual (RM6000)",
+            "Education fees (RM7000)",
+            "Medical Expenses on Serious Disease (RM8000)",
+            "Lifestyle (RM2500)",
+            "Lifestyle on personal computer, smartphone or table (RM2500)",
+            "Breastfeeding Equipment (RM1000)",
+            "Child Care Fee (RM3000)",
+            "SSPN (RM8000)",
+            "Husband/ Wife/ Alimony Payments (RM4000)",
+            "Disabled spouse (RM5000)",
+            "Education fee for children (RM8000)",
+            "Disabled Child (RM6000)",
+            "EPF (RM7000)",
+            "PRS (RM3000)",
+            "Education or Medical Insurance (RM3000)",
+            "SOCSO (RM250)",
+            "Lifestyle on Sport Equipment (RM500)",
+            "Domestic Travel (RM1000)"
+    );
 
     public void setAnnualIncome(double annualIncome) {
         this.annualIncome = annualIncome;
@@ -54,10 +54,10 @@ public class IncomeTax {
         this.taxSlab = taxSlab;
     }
 
-//    public List<String> getTaxClaimOptions() {
-//        return taxClaimOptions;
-//    }
-//
+    public List<String> getTaxClaimOptions() {
+        return taxClaimOptions;
+    }
+
 //    public void setTaxClaimOptions(List<String> taxClaimOptions) {
 //        this.taxClaimOptions = taxClaimOptions;
 //    }
@@ -72,7 +72,7 @@ public class IncomeTax {
 
         return "{\"taxAmount\": " + taxAmount +
                 ", \"taxSlab\": \"" + taxSlab + "\"" +
-//                "\"taxClaimOptions\"" + taxClaimOptions +
+                ", \"taxClaimOptions\": " + taxClaimOptions +
                 '}';
     }
 }
